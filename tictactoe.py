1117dd5956a1442e86ab0de3e0ffac1a1ae7dac7
emet12381 <mirskyemmet@gmail.com>
2025-07-31 17:12:38 +0000

    7/31/25

--- tictactoe.py
+++ tictactoe.py
@@ -8,13 +8,52 @@ for i in range (3):
 
 player = "X"
 moves = 0
+print (board [0][0]+" | "+ board [0][1]+ " | "+ board [0][2])
+print("---------")
+print (board [1][0]+ " | "+ board [1][1]+" | "+ board [1][2])
+print ("---------")
+print (board [2][0]+ " | "+ board [2][1]+" | "+ board [2][2])
 while (moves >=0 and moves <9):
+    row = int(input ("what row do you want to play in?"))
+    col = int(input ("what column do you want to play in?"))
+    board [row][col] = player
+    moves = moves+1
+    
+    if board [0][0] == player and board [0][1] == player and board [0][2] == player:
+        print("player", player, "wins!")
+        moves = -1
+    elif board [1][0] == player and board [1][1] == player and board [1][2] == player:
+        print("player", player, "wins!")
+        moves = -1
+    elif board [2][0] == player and board [2][1] == player and board [2][2] == player:
+        print("player", player, "wins!")
+        moves = -1
+    elif board [0][0] == player and board [1][0] == player and board [2][0] == player:
+        print("player", player, "wins!")
+        moves = -1
+    elif board [0][1] == player and board [1][1] == player and board [2][1] == player:
+        print("player", player, "wins!")
+        moves = -1
+    elif board [0][2] == player and board [1][2] == player and board [2][2] == player:
+        print("player", player, "wins")
+        moves = -1
+    elif board [0][0] == player and board [1][1] == player and board [2][2] == player:
+        print("player", player, "wins!")
+        moves = -1
+    elif board [0][2] == player and board [1][1] == player and board [2][0] == player:
+        print("player", player, "wins!")
+        moves = -1
+    if moves == 9:
+       print("it's a tie") 
+       break
     print (board [0][0]+" | "+ board [0][1]+ " | "+ board [0][2])
     print("---------")
     print (board [1][0]+ " | "+ board [1][1]+" | "+ board [1][2])
     print ("---------")
     print (board [2][0]+ " | "+ board [2][1]+" | "+ board [2][2])
-    row = int(input ("what row do you want to play in?"))
-    col = int(input ("what column do you want to play in?"))
-    board [row][col] = player
-    moves = moves+1
+ 
+
+    if player == "X":
+        player = "O"
+    elif player == "O":
+        player = "X"
