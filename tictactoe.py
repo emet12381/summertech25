2d754ec90f1e29e1e78ca98e8c0bfee6197e75b7
emet12381 <mirskyemmet@gmail.com>
2025-07-31 15:12:28 +0000

    7/31/25

---
+++ tictactoe.py
@@ -0,0 +1,21 @@
+board = []
+for i in range (3):
+    slot = []
+    for i in range (3):
+        slot.append(" ")
+    board.append(slot)
+    
+
+player = "X"
+moves = 0
+print (board [0][0]+" | "+ board [0][1]+ " | "+ board [0][2])
+print("---------")
+print (board [1][0]+ " | "+ board [1][1]+" | "+ board [1][2])
+print ("---------")
+print (board [2][0]+ " | "+ board [2][1]+" | "+ board [2][2])
+
+
+row = int(input ("what row do you want to play in?"))
+col = int(input ("what column do you want to play in?"))
+board [row][col] = player
+moves = moves+1
