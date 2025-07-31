2ec5df107b86f43d6176bb72f9d3d3cc02fb3562
emet12381 <mirskyemmet@gmail.com>
2025-07-31 14:43:16 +0000

    7/31/25

--- shoppinglist.py
+++ shoppinglist.py
@@ -4,10 +4,27 @@ cart = []
 # cart.append("pineapple")
 # cart.append("parsley")
 # print (cart)
+
+
+
+
 while(True):
-    item = input ("what would you like to add?")
-    cart.append (item)
-    print (cart)
-    answer = input ("would you like to add another item?")
+    answer = input ("press a if you would like to add an item and press r if you would like to remove item")
+    if answer == ("a"):
+        item = input ("what would you like to add?")
+        cart.append (item)
+        print (cart)
+    elif answer == ("r"):
+        if len (cart) == 0:
+            print ("the cart is empty")
+            continue
+        item = input("what would you like to remove?")
+        cart.remove (item)
+        print (cart)
+    else:
+        print ("invalid option")
+        continue
+    answer = input ("would you like to add or remove another item?")
     if answer == ("no"):
         break
+     
